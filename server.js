e483484ccadbee00f89f8d2cfdfcf3b721eaa585
heath smith <heathsmith73@gmail.com>
2017-12-14 10:22:00 -0800

    added the update/delete code

--- server.js
+++ server.js
@@ -18,8 +18,6 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-// app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
-
 // gets all the books from BD.
 
 app.get('/api/v1/books', (request, response) => {
@@ -54,6 +52,31 @@ app.post('/api/v1/books', (request, response) => {
       response.send('insert complete');
     }
   )
+});
+
+// updating the seleceted book
+
+app.put('/api/v1/books/:id', (request, response) => {
+  client.query(`
+    UPDATE books
+    SET title=$1, author=$2, isbn=$3, image_url=$4, description=$5
+    WHERE book_id=$6
+    `,
+    [request.body.title, request.body.author, request.body.isbn, request.body.image_url, request.body.description, request.body.book_id]
+  )
+    .then(() => response.send(200))
+    .catch(console.error);
+});
+
+// delete a single book
+
+app.delete('/api/v1/books/:id', (request, response) => {
+  client.query(
+    `DELETE FROM books WHERE book_id=$1;`,
+    [request.params.id]
+  )
+    .then(() => response.send(204))
+    .catch(console.error);
 });
 
 
