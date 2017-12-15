ebd6e5e81e4b2844cbfb0bef5aef4ba44a4f1ccf
Dean <dean.murphy90@gmail.com>
2017-12-15 10:32:17 -0800

    added api and get for search

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const cors = require('cors');
 const pg = require('pg');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const superagent = require('superagent');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const CLIENT_URL = process.env.CLIENT_URL;
 const TOKEN = process.env.TOKEN;
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 
 const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
@@ -25,6 +27,33 @@ app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
 // admin get
 app.get('api/v1/admin', (req, res) => res.send(TOKEN === parseInt(req.query.token)))
 
+// api get
+app.get('/api/v1/books/find', (req, res) => {
+  let url = 'https://www.googleapis.com/books/v1/volumes';
+  let query = ''
+  if(req.query.title) query += `+intitle:${req.query.title}`;
+  if(req.query.author) query += `+inauthor:${req.query.author}`;
+  if(req.query.isbn) query += `+isbn:${req.query.isbn}`;
+
+  superagent.get(url)
+    .query({'q': query})
+    .query({'key': API_KEY})
+    .then(response => response.body.items.map((book, idx) => {
+      let { title, authors, industryIdentifiers, imageLinks, description } = book.volumeInfo;
+      let placeholderImage = 'http://www.newyorkpaddy.com/images/covers/NoCoverAvailable.jpg';
+
+      return {
+        title: title ? title : 'No title available',
+        author: authors ? authors[0] : 'No authors available',
+        isbn: industryIdentifiers ? `ISBN_13 ${industryIdentifiers[0].identifier}` : 'No ISBN available',
+        image_url: imageLinks ? imageLinks.smallThumbnail : placeholderImage,
+        description: description ? description : 'No description available',
+        book_id: industryIdentifiers ? `${industryIdentifiers[0].identifier}` : '',
+      }
+    }))
+    .then(arr => res.send(arr))
+    .catch(console.error)
+})
 
 
 // gets all the books from BD.
